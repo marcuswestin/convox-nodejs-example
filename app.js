0f81a671ff39693d57fca0dd76c3a0a8562471a9
Marcus Westin <marcus.westin@gmail.com>
2020-05-16 10:13:45 -0400

    test change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World 3!\nI\'m: ' + process.env.APP + '\nBuild: ' + process.env.BUILD + '\nRelease: ' + process.env.RELEASE + '\nrunning on: ' + process.env.RACK + '\nUA: '+req.headers['user-agent']);
+  res.end('Hello again!\nI\'m: ' + process.env.APP + '\nBuild: ' + process.env.BUILD + '\nRelease: ' + process.env.RELEASE + '\nrunning on: ' + process.env.RACK + '\nUA: '+req.headers['user-agent']);
   console.log('PING 4', req.url);
 });
 
