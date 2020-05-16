ccfa185dbe30c2adf05d1adfe8dd4ee55a3772a8
Marcus Westin <marcus.westin@gmail.com>
2020-05-16 08:57:09 -0400

    Some test changes

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,11 @@ FROM node:10.16.3-alpine
 
 WORKDIR /usr/src/app
 
+# RUN npm install nodemon -g
+
 COPY . /usr/src/app
 
 EXPOSE 3000
 
+# CMD ["nodemon", "app.js"]
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const port = process.env.PORT
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Convox!\nI\'m: ' + process.env.APP + '\nBuild:' + process.env.BUILD + '\nRelease: ' + process.env.RELEASE + '\nrunning on: ' + process.env.RACK);
-  console.log('PING');
+  res.end('Hello World 3!\nI\'m: ' + process.env.APP + '\nBuild: ' + process.env.BUILD + '\nRelease: ' + process.env.RELEASE + '\nrunning on: ' + process.env.RACK + '\nUA: '+req.headers['user-agent']);
+  console.log('PING 4', req.url);
 });
 
 server.listen(port, () => {
